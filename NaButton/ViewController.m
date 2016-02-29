1b052352bc8b84f8b7ef59ee66f0f64fc0574de4
LJH.Hexo <89713084@qq.com>
2016-02-29 14:43:59 +0800

    commit by ```

--- NaButton/ViewController.m
+++ NaButton/ViewController.m
@@ -6,9 +6,9 @@
 //  Copyright © 2016年 LJH. All rights reserved.
 //
 
-#import "ViewController.h"
 #import "NAStatusButton.h"
 #import "NAThirdOrderBerzier.h"
+#import "ViewController.h"
 
 @interface ViewController ()
 
@@ -17,33 +17,28 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    NAStatusButton *bt = [NAStatusButton buttonWithNaButtonType:NAStatusButtonNormal];
-    bt.na_StatusButtontouchUpInsideBlock = ^(NAStatusButton *bt){
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+  NAStatusButton *bt =
+      [NAStatusButton buttonWithNaButtonType:NAStatusButtonNormal];
+  bt.na_StatusButtontouchUpInsideBlock = ^(NAStatusButton *bt) {
 
-    };
-    bt.frame = CGRectMake(100, 100, 88, 44);
-    [self.view addSubview:bt];
-    
-//    NSArray *ar;
-//    [ar enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        *stop = YES;
-//    }];
-    
-//    [path closePath];
-    
-    
-   
-    
+  };
+  bt.frame = CGRectMake(100, 100, 200, 200);
+  [self.view addSubview:bt];
+
+  //    NSArray *ar;
+  //    [ar enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL
+  //    * _Nonnull stop) {
+  //        *stop = YES;
+  //    }];
+
+  //    [path closePath];
 }
 
-
-
-
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 @end
